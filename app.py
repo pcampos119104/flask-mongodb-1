081acf190bcb34a7acc37827abc3116e945d8a30
Pedro Campos <pcampos@localhost.localdomain>
2020-05-18 07:29:43 -0300

    first commit

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from pymongo import MongoClient
 
 app = Flask(__name__)
 api = Api(app)
-@app.route('/hello')
-def hello_world():
-    return 'hello'
 
-class Add(Resource):
+client = MongoClient('mongodb://db:27017')
+db = client.SentenceDatabase
+users = db['Users']
+
+class Hello(Resource):
     def get(self):
         return 'hello', 200
 
-api.add_resource(Add, '/hello2')
+api.add_resource(Add, '/hello')
